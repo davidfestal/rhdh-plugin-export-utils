4ef5d5c94355aa4f69fc681e9ebe781d8f46b8f7
David Festal <dfestal@redhat.com>
2025-01-30 18:59:06 +0100

    f
    Signed-off-by: David Festal <dfestal@redhat.com>

--- export-dynamic/export-dynamic.sh
+++ export-dynamic/export-dynamic.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 errors=''
-packDestination=${INPUTS_DESTINATION}
-mkdir -pv ${packDestination}
 IFS=$'\n'
 for plugin in $(cat ${INPUTS_PLUGINS_FILE})
 do
@@ -24,17 +22,11 @@ do
     if [[ "$(grep -e '"role" *: *"frontend-plugin' package.json)" != "" ]]
     then
         pluginType=frontend
-        args="$args --no-in-place"
-        
         optionalScalprumConfigFile="$(dirname ${INPUTS_PLUGINS_FILE})/${pluginPath}/${INPUTS_SCALPRUM_CONFIG_FILE_NAME}"
         if [ -f "${optionalScalprumConfigFile}" ]
         then
             args="$args --scalprum-config ${optionalScalprumConfigFile}"
         fi
-        
-    else
-        pluginType=backend
-        args="$args --embed-as-dependencies"
     fi
     
     echo "========== Exporting $pluginType plugin $pluginPath =========="
@@ -45,7 +37,7 @@ do
         echo "  copying source overlay"
         cp -Rfv ${optionalSourceOverlay}/* .
     fi
-    
+
     set +e
     echo "  running the 'export-dynamic-plugin' command with args: $args"
     echo "$args" | xargs npx --yes @janus-idp/cli@${INPUTS_JANUS_CLI_VERSION} package export-dynamic-plugin
@@ -58,11 +50,11 @@ do
     fi
 
     # package the dynamic plugin in a container image
-    if [ "${INPUTS_BASE_IMAGE_TAG_NAME}" ]
+    if [[ "${INPUTS_IMAGE_REPOSITORY_PREFIX}" != "" ]]
     then
         PLUGIN_NAME=$(grep -o  '"name":\s*".*"' package.json  | cut -d' ' -f2-| sed 's/"//g; s/\//-/g; s/@//g')
         PLUGIN_VERSION=$(grep -o  '"version":\s*".*"' package.json  | cut -d' ' -f2- | sed 's/"//g; s/^://g')
-        PLUGIN_CONTAINER_TAG="${INPUTS_BASE_IMAGE_TAG_NAME}/${PLUGIN_NAME}:${PLUGIN_VERSION}"
+        PLUGIN_CONTAINER_TAG="${INPUTS_IMAGE_REPOSITORY_PREFIX}/${PLUGIN_NAME}:${PLUGIN_VERSION}"
         echo "========== Packaging Container ${PLUGIN_CONTAINER_TAG} =========="
 
         npx --yes @janus-idp/cli@${INPUTS_JANUS_CLI_VERSION} package package-dynamic-plugins --tag $PLUGIN_CONTAINER_TAG
@@ -75,27 +67,33 @@ do
         fi
     fi
 
-    echo "  running npm pack on the exported './dist-dynamic' sub-folder"
-    json=$(npm pack ./dist-dynamic --pack-destination $packDestination --json)
-    if [ $? -ne 0 ]
+    if [[ "${INPUTS_DESTINATION}" != "" ]]
     then
-        errors="${errors}\n${pluginPath}"
+        packDestination=${INPUTS_DESTINATION}
+        mkdir -pv ${packDestination}
+
+        echo "  running npm pack on the exported './dist-dynamic' sub-folder"
+        json=$(npm pack ./dist-dynamic --pack-destination $packDestination --json)
+        if [ $? -ne 0 ]
+        then
+            errors="${errors}\n${pluginPath}"
+            set -e
+            popd > /dev/null
+            continue
+        fi
         set -e
+        
+        filename=$(echo "$json" | jq -r '.[0].filename')
+        integrity=$(echo "$json" | jq -r '.[0].integrity')
+        echo "$integrity" > $packDestination/${filename}.integrity
+        optionalConfigFile="$(dirname ${INPUTS_PLUGINS_FILE})/${pluginPath}/${INPUTS_APP_CONFIG_FILE_NAME}"
+        if [ -f "${optionalConfigFile}" ]
+        then
+            echo "  copying default app-config"
+            cp -v "${optionalConfigFile}" "$packDestination/${filename}.${INPUTS_APP_CONFIG_FILE_NAME}"
+        fi
         popd > /dev/null
-        continue
     fi
-    set -e
-    
-    filename=$(echo "$json" | jq -r '.[0].filename')
-    integrity=$(echo "$json" | jq -r '.[0].integrity')
-    echo "$integrity" > $packDestination/${filename}.integrity
-    optionalConfigFile="$(dirname ${INPUTS_PLUGINS_FILE})/${pluginPath}/${INPUTS_APP_CONFIG_FILE_NAME}"
-    if [ -f "${optionalConfigFile}" ]
-    then
-        echo "  copying default app-config"
-        cp -v "${optionalConfigFile}" "$packDestination/${filename}.${INPUTS_APP_CONFIG_FILE_NAME}"
-    fi
-    popd > /dev/null
 done
 echo "Plugins with failed exports: $errors"
 echo "ERRORS=$errors" >> $GITHUB_OUTPUT
